de1bf6da256e56726af05bdf59a86b7871308a94
Core <seorisachs@gmail.com>
2014-06-23 14:29:55 -0400

    added more circle functionality

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -2,12 +2,14 @@
 
 int CircleX;// instantiating variables, where it will start and where it will go to
 int CircleY;
+int CircleRadius;
 
 //--------------------------------------------------------------
 void testApp::setup(){
  
     CircleX =300;
     CircleY=200;
+    CircleRadius=25;
 
 }
 
@@ -23,7 +25,7 @@ void testApp::draw(){
     CircleX++; //moves off the right slowly
     
     ofSetColor(255, 0, 0); //circle color
-    ofCircle(mouseX,mouseY,50); //basic circle movement with mouse
+    ofCircle(mouseX,mouseY,CircleRadius); //basic circle movement with mouse
     
     
 }
@@ -35,7 +37,6 @@ void testApp::keyPressed(int key){
 
 //--------------------------------------------------------------
 void testApp::keyReleased(int key){
-    
     
 
 }
@@ -52,11 +53,14 @@ void testApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button){
+    //CircleRadius++; //everytime you press mouse, the radius gets +1 bigger
+    
 
 }
 
 //--------------------------------------------------------------
 void testApp::mouseReleased(int x, int y, int button){
+    
 
 }
 
